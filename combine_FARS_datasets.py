e522aada2c064bbf25b3c10e1d487e3d04647b15
Mira Flynn <miraflynn0@gmail.com>
2022-11-09 09:36:43 -0500

    Rewriting and simplifying code in combine_FARS_datasets.py

--- combine_FARS_datasets.py
+++ combine_FARS_datasets.py
@@ -5,7 +5,8 @@ import os
 
 def combine_FARS_datasets(path='FARS CSVs/', output_filename='combined_FARS.csv'):
     all_filenames = get_all_csv_filenames(path)
-    df = load_and_combine_to_dataframe(all_filenames)
+    all_files = get_all_dfs_from_csv(all_filenames)
+    df = concat_pandas_dfs(all_files)
     write_to_file(df, output_filename)
 
 
@@ -16,30 +17,22 @@ def get_all_csv_filenames(path):
     all_filenames = glob.glob(os.path.join(path, "*.CSV"))
     return all_filenames
 
-
-def load_and_combine_to_dataframe(filenames):
-
+def get_all_dfs_from_csv(filenames, only_include_df_with_lat_lon=True):
     all_files = filenames
 
 
     dfs=[]
 
     for filename in all_files:
-        # df = pd.read_csv(filename, index_col=None, encoding_errors='ignore', dtype={'LATITUDE': float, 'LONGITUD': float})
-        # if('LATITUDE' in df.columns and 'LONGITUD' in df.columns):
-        #     li.append(df[['LATITUDE','LONGITUD']])
-        # # print("reloading lol")
-
         df=pd.read_csv(filename,index_col=None, encoding_errors='ignore', low_memory=False)
-        if('LATITUDE' in df.columns and 'LONGITUD' in df.columns):
+        if(('LATITUDE' in df.columns and 'LONGITUD' in df.columns) or only_include_df_with_lat_lon):
             dfs.append(df)
-    # return dfs
-    return pd.concat(dfs, axis=0, ignore_index=True)
-        
-
-
+    return dfs
     
 
+def concat_pandas_dfs(dfs):
+    return pd.concat(dfs, axis=0, ignore_index=True)
+        
 
 if __name__=="__main__":
     combine_FARS_datasets()
