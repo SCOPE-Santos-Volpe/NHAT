b40acf249783aa3f598b34b8886bea800f488eab
Mira Flynn <miraflynn0@gmail.com>
2022-11-09 09:54:48 -0500

    Update combine_FARS_datasets.py with type hints (unenforced) for functions

--- combine_FARS_datasets.py
+++ combine_FARS_datasets.py
@@ -1,25 +1,24 @@
 import glob
 import pandas as pd
-import numpy as np
+# import numpy as np
 import os
 
-def combine_FARS_datasets(path='FARS CSVs/', output_filename='combined_FARS.csv'):
+def combine_FARS_datasets(path: str = 'FARS CSVs/', output_filename: str = 'combined_FARS.csv') -> pd.DataFrame:
     all_filenames = get_all_csv_filenames(path)
     all_files = get_all_dfs_from_csv(all_filenames)
     df = concat_pandas_dfs(all_files)
     write_to_file(df, output_filename)
+    return df
 
+def write_to_file(df: pd.DataFrame, filename: str):
+    return df.to_csv(filename)
 
-def write_to_file(df, filename):
-    df.to_csv(filename)
-
-def get_all_csv_filenames(path):
+def get_all_csv_filenames(path: str) -> list[str]:
     all_filenames = glob.glob(os.path.join(path, "*.CSV"))
     return all_filenames
 
-def get_all_dfs_from_csv(filenames, only_include_df_with_lat_lon=True):
+def get_all_dfs_from_csv(filenames: list[str], only_include_df_with_lat_lon: bool=True) -> list[pd.DataFrame]:
     all_files = filenames
-
 
     dfs=[]
 
@@ -27,10 +26,10 @@ def get_all_dfs_from_csv(filenames, only_include_df_with_lat_lon=True):
         df=pd.read_csv(filename,index_col=None, encoding_errors='ignore', low_memory=False)
         if(('LATITUDE' in df.columns and 'LONGITUD' in df.columns) or only_include_df_with_lat_lon):
             dfs.append(df)
+
     return dfs
     
-
-def concat_pandas_dfs(dfs):
+def concat_pandas_dfs(dfs: list[pd.DataFrame]) -> pd.DataFrame:
     return pd.concat(dfs, axis=0, ignore_index=True)
         
 
