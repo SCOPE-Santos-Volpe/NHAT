aa027134131d7a9929138c0e695bbb1546c657c5
Jackie Zeng <dolphin.pool01@gmail.com>
2022-11-16 01:20:04 -0500

    add connector

--- AWS/database_connector.py
+++ AWS/database_connector.py
@@ -1,4 +1,6 @@
 import psycopg2
+import pandas as pd
+
 connection = psycopg2.connect(
     host = 'database-1.ci75bfibgs4e.us-east-1.rds.amazonaws.com',
     port = 5432,
@@ -9,26 +11,39 @@ connection = psycopg2.connect(
 
 # cursor object to interact and execute the commands on the DB
 cursor=connection.cursor()
+print("connected to DB")
 
-# Create tables
-cursor.execute("""CREATE TABLE FARS(
-id SERIAL PRIMARY KEY,
-name text,
-sex text,
-age float,
-sibsp integer,
-parch integer)""")
+# # Create tables - THESE ARE FAKE FILLER TABLES RN
+# cursor.execute("""CREATE TABLE FARS(
+# id SERIAL PRIMARY KEY,
+# name text,
+# sex text,
+# age float,
+# sibsp integer,
+# parch integer)""")
 
-cursor.execute("""CREATE TABLE survival(
-id SERIAL PRIMARY KEY,
-survived integer)""")
+# cursor.execute("""CREATE TABLE survival(
+# id SERIAL PRIMARY KEY,
+# survived integer)""")
 
-cursor.execute("""CREATE TABLE tripInfo(
-id SERIAL PRIMARY KEY,
-pclass integer,
-ticket text,
-fare float,
-cabin text,
-embarked text)""")
+# cursor.execute("""CREATE TABLE tripInfo(
+# id SERIAL PRIMARY KEY,
+# pclass integer,
+# ticket text,
+# fare float,
+# cabin text,
+# embarked text)""")
 
-connection.commit()
+# connection.commit()
+
+
+# using pandas to execute SQL queries
+#TODO: Need to switch this command to SQL Alchemy
+
+sql = """
+SELECT "table_name","column_name", "data_type", "table_schema"
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE "table_schema" = 'public'
+ORDER BY table_name  
+"""
+pd.read_sql(sql, con=connection)
