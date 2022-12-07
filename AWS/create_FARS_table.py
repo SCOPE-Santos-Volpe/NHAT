185dd4a74e500e5f28bda3b75e90aadf971f6292
Jackie Zeng <dolphin.pool01@gmail.com>
2022-12-07 10:28:35 -0500

    script that adds FARS table to database

--- AWS/create_FARS_table.py
+++ AWS/create_FARS_table.py
@@ -8,6 +8,7 @@ import pandas as pd
 import psycopg2
 from config.config import config
 from sqlalchemy import create_engine
+import utils
 
 # Establish sqlalchemy connection
 conn_string = 'postgresql://scope_team:greenTea123@database-1.ci75bfibgs4e.us-east-1.rds.amazonaws.com/FARS'
@@ -22,29 +23,10 @@ conn.autocommit = True
 cursor = conn.cursor()
 print('Python connected to PostgreSQL via Psycogp2!')
 
+#------------------------------------------------------------
 # Create FARS dataframe
-df = pd.read_csv("FARS/combined_FARS.csv") 
-
-# Get the first 10 rows to test
-df_trimmed = df.head(10)
-print(df_trimmed)
+fars = utils.load_df_from_csv(path='combined_FARS.csv', low_memory = False)
 
 # Load the FARS data into AWS
-df_trimmed.to_sql('data', con=sqlalchemy_conn, if_exists='replace',
+fars.to_sql('FARS(2015-2020)', con=sqlalchemy_conn, if_exists='replace',
           index=False)
-
-# # Create table
-# cur = con.cursor()
-# cur.execute("""
-# CREATE TABLE customer(
-# customer_id INT PRIMARY KEY NOT NULL,
-# name CHAR(50) NOT NULL,
-# address CHAR(100),
-# email CHAR(50),
-# phone_number CHAR(20));
-# """)
-# print('Table created in PostgreSQL')
-
-# # Close the connection
-# con.commit()
-# con.close()
